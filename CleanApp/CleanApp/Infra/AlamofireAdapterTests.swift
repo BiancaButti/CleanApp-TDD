d4efe25c7ddb42e514bcba453445df2072bf7e9b
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2023-08-30 13:38:40 -0300

    feat: add infra layer

---
+++ CleanApp/CleanApp/Infra/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  AlamofireAdapterTests.swift
+//  DataLayerTests
+//
+//  Created by Bianca on 30/08/23.
+//
+
+import XCTest
+
+final class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+    }
+
+}
