0fde1591f0b448e62f2e4329cc85e45469080310
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2023-08-31 09:34:06 -0300

    feat: create helper to improve code readability

--- CleanApp/CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -42,19 +42,7 @@ class AlamofireAdapterTests: XCTestCase {
     }
  
     func test_post_should_complete_with_error_when_request_completes_with_error() {
-        let sut = makeSut()
-        UrlProtocolStub.simulate(data: nil, response: nil, error: makeError())
-        let exp = expectation(description: "waiting")
-        sut.post(to: makeUrl(), with: makeValidData()) { result  in
-            switch result {
-            case .success:
-                XCTFail("Expected error got \(result) instead")
-            case .failure(let error):
-                XCTAssertEqual(error, .noConnectivity)
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1)
+        expectResult(.failure(.noConnectivity), when: (data: nil, response: nil, error: makeError()))
     }
     
 }
@@ -77,6 +65,24 @@ extension AlamofireAdapterTests {
         UrlProtocolStub.observerRequest { request = $0 }
         wait(for: [exp], timeout: 1)
         action(request!)
+    }
+    
+    func expectResult(_ expectedResult: Result<Data, HttpError>, when stub: (data: Data?, response: HTTPURLResponse?, error: Error?), file: StaticString = #file, line: UInt = #line) {
+        let sut = makeSut()
+        UrlProtocolStub.simulate(data: stub.data, response: stub.response, error: stub.error)
+        let exp = expectation(description: "waiting")
+        sut.post(to: makeUrl(), with: makeValidData()) { receivedResult  in
+            switch (expectedResult, receivedResult) {
+            case (.failure(let expectedError), .failure(let receivedError)):
+                XCTAssertEqual(expectedError, receivedError, file: file, line: line)
+            case (.success(let expectedData), .success(let receivedData)):
+                XCTAssertEqual(expectedData, receivedData)
+            default:
+                XCTFail("Expected x got y instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 
