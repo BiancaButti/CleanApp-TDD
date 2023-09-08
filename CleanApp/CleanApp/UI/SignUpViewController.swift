7d18f2541cf4572a820f187f0d6e05a493c4e599
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2023-09-08 15:32:32 -0300

    feat: ensure signupviewconroller has a loading indicator and its hidden by default

---
+++ CleanApp/CleanApp/UI/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Bianca on 08/09/23.
+//
+
+import Foundation
+import UIKit
+
+final class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
