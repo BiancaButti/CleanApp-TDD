2b11f3106cf9c526f9d0f9722fce0b4968ea6fbb
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2023-09-08 15:11:01 -0300

    feat: allow all targets to run oh both ios and macos

---
+++ CleanApp/CleanApp/UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewControllerTests.swift
+//  SignUpViewControllerTests
+//
+//  Created by Bianca on 08/09/23.
+//
+
+import XCTest
+import PresentationLayer
+
+final class SignUpViewControllerTests: XCTestCase {
+
+    func test_() {
+        
+    }
+}
