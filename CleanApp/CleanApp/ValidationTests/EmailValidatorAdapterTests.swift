d3becbf92b6847853ba0d2c05d8baf78c7a7dea5
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2023-11-22 14:45:19 -0300

    feat: add validation target

---
+++ CleanApp/CleanApp/ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  EmailValidatorAdapterTests
+//
+//  Created by Bianca on 22/11/23.
+//
+
+import XCTest
+
+final class EmailValidatorAdapterTests: XCTestCase {
+
+    func testExample() {
+        
+    }
+
+}
