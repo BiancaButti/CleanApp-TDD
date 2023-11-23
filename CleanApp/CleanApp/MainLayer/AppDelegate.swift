8e3d077d5330706fafa13956a8eb6ce4ddeb215e
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2023-11-23 09:39:55 -0300

    feat: add main target

---
+++ CleanApp/CleanApp/MainLayer/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MainLayer
+//
+//  Created by Bianca on 23/11/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
